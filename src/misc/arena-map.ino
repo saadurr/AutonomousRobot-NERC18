0da4beef9f679f063cfde3db41cffc1fd3904033
Saad Ur Rehman <18420434+saadurr@users.noreply.github.com>
2021-10-19 22:08:44 +0500

    Create arena-map.ino

---
+++ src/misc/arena-map.ino
@@ -0,0 +1,226 @@
+  //0 --> black
+  //1 --> white
+  //2 ---> blue
+  //3 --> red
+
+  //read the colors cw
+
+
+  if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'blue')
+  {
+    //position 0 and orientation 0 - facing outwards from wall, row 1, first point
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'blue' && colorVal_left == 'red')
+  {
+    //position 0 and orientation 1  ---- turned cw 90
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'blue' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 0 and orientation 2  ---- turned cw 90
+  }
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 0 and orientation 3 ---- turned cw90
+  }
+
+
+
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'red' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 1 and orientation 0
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'blue')
+  {
+    //position 1 and orientation 1 --- turned cw90
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'blue' && colorVal_left == 'red')
+  {
+    //position 1 and orientation 2 -- turned cw90
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'blue' && colorVal_back == 'red' && colorVal_left == 'red')
+  {
+    //position 1 and orientation 3
+  }
+
+
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'blue' && colorVal_left == 'black')
+  {
+    //position 2 and orientation 0
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'blue' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 2 and orientation 1
+  }
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 2 and orientation 2
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'blue')
+  {
+    //position 2 and orientation 3
+  }
+
+
+
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 3 and orientation 0
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'blue')
+  {
+    //position 3 and orientation 1
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'blue' && colorVal_left == 'red')
+  {
+    //position 3 and orientation 2
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'blue' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 3 and orientation 3
+  }
+
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'blue' && colorVal_left == 'red')
+  {
+    //position 4 and orientation 0    ---last position in row 1
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'blue' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 4 and orientation 1    ---cw90
+  }
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 4 and orientation 2    ---cw90
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'blue')
+  {
+    //position 4 and orientation 3    ---cw90
+  }
+
+
+
+  else if (colorVal_front == 'red' && colorVal_right == 'blue' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 5 and orientation 0    ---last point on row 2
+  }
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'red')
+  {
+    //position 5 and orientation 1    ---cw90
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'red' && colorVal_left == 'blue')
+  {
+    //position 5 and orientation 2
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'red' && colorVal_back == 'blue' && colorVal_left == 'black')
+  {
+    //position 5 and orientation 3
+  }
+
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'red' && colorVal_left == 'red')
+  {
+    //position 6 and orientation 0
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'red' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 6 and orientation 1
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 6 and orientation 2
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'red')
+  {
+    //position 6 and orientation 3
+  }
+
+
+
+
+  else if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 7 and orientation 0
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'red')
+  {
+    //position 7 and orientation 1
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 7 and orientation 2
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 7 and orientation 3
+  }
+
+
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'blue' && colorVal_left == 'red')
+  {
+    //position 8 and orientation 0
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'blue' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 8 and orientation 1
+  }
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 8 and orientation 2
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'blue')
+  {
+    //position 8 and orientation 3
+  }
+
+
+
+  else if (colorVal_front == 'blue' && colorVal_right == 'red' && colorVal_back == 'black' && colorVal_left == 'black')
+  {
+    //position 9 and orientation 0
+  }
+
+  else if (colorVal_front == 'red' && colorVal_right == 'black' && colorVal_back == 'black' && colorVal_left == 'blue')
+  {
+    //position 9 and orientation 1
+  }
+
+  else if (colorVal_front == 'black' && colorVal_right == 'black' && colorVal_back == 'blue' && colorVal_left == 'red')
+  {
+    //position 9 and orientation 2
+  }
+
+  else  if (colorVal_front == 'black' && colorVal_right == 'blue' && colorVal_back == 'red' && colorVal_left == 'black')
+  {
+    //position 9 and orientation 3
+  }
